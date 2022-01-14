8c5f1398512583663c87b5dce93b4b6ecc2ef0d2
Amaan18 <amaangokak18@gmail.com>
2022-01-14 00:41:36 -0800

    remove rss

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,11 @@
-import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
-
+import 'nextra-theme-blog/style.css'
 import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
         <link
           rel="preload"
           href="/fonts/Inter-roman.latin.var.woff2"
